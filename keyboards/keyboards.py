915f11ca81bfb621955ee133aee4d421e73b5655
Mikhail Trifonov <trifonov2812@gmail.com>
2023-06-05 23:10:54 +0300

    added lexicon links, buttons for modes and pdf mode

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,10 +1,23 @@
-FEEDBACK_BUTTONS: dict[str, str] = {
+from lexicon.lexicon import LEXICON_RU
+
+FEEDBACK_BTNS: dict[str, dict[str, str]] = {
     'me': {
-            'text': 'Мой создатель',
+            'text': LEXICON_RU['creator'],
             'url': 'tg://user?id=1062363884'
         },
     'botlink': {
-            'text': 'Бот Verto',
+            'text': LEXICON_RU['bot_name'],
             'url': 'tg://user?id=6185798537'
         }
+}
+
+MODE_BTNS: dict[str, str] = {
+    'PDF': 'pdf_mode',
+}
+
+# Заменить на ссылки на LEXICON
+PDF_MODES_BTNS: dict[str, str] = {
+    LEXICON_RU['combine_pdf']: 'combine',
+    LEXICON_RU['divide_pdf']: 'divide',
+    LEXICON_RU['watermark_pdf']: 'watermark',
 }
