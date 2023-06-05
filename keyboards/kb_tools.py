6d8de690890c622e2320d5f355542929b6bfea07
Mikhail Trifonov <trifonov2812@gmail.com>
2023-06-05 23:13:02 +0300

    added creating keyboards for feedback and choosing file modes

--- keyboards/kb_tools.py
+++ keyboards/kb_tools.py
@@ -8,37 +8,21 @@ def _create_url_inline_btn(text: str, url_input: str):
                                 url=url_input)
 
 
-
-def create_inline_kb(width: int,
-                     *args: str,
-                     last_btn: bool | None = None,
-                     **kwargs: str) -> InlineKeyboardMarkup:
-
+def feedback_kb(width: int, **btns: str):
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
-
     buttons: list[InlineKeyboardButton] = []
-
-    # Заполняем список кнопками из аргументов args и kwargs
-    if args:
-        for button in args:
-            buttons.append(InlineKeyboardButton(
-                text=LEXICON_RU[button] if button in LEXICON_RU else button,
-                callback_data=button))
-    if kwargs:
-        for button, text in kwargs.items():
-            if type(text) == dict:
-                buttons.append(_create_url_inline_btn(text['text'], text['url']))
-            else:
-                buttons.append(InlineKeyboardButton(
-                    text=text,
-                    callback_data=button))
-
+    for text, data in btns.items():
+        buttons.append(_create_url_inline_btn(data['text'], data['url']))
     kb_builder.row(*buttons, width=width)
-
-    if last_btn:
-        kb_builder.row(InlineKeyboardButton(
-                            text=LEXICON_RU['last_btn'],
-                            callback_data='last_btn'))
-
     return kb_builder.as_markup()
 
+
+def choose_file_mode_kb(width: int, **btns: str):
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+    for text, data in btns.items():
+        buttons.append(InlineKeyboardButton(
+                            text=text,
+                            callback_data=data))
+    kb_builder.row(*buttons, width=width)
+    return kb_builder.as_markup()
